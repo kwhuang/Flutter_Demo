dc7f959498b46945b877cde4b297978402285d69
黄凯伟 <huangkaiwei@hengqijy.com>
2020-01-13 17:45:01 +0800

    创建TabBarDemo类

---
+++ flutter_widget_demo/lib/widget_demo/CustermerTabBarDemo.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CusturmerTabBarDemo extends StatefulWidget {
+  @override
+  _CusturmerTabBarDemoState createState() => _CusturmerTabBarDemoState();
+}
+
+class _CusturmerTabBarDemoState extends State<CusturmerTabBarDemo> {
+
+  List<String> _tabs;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
